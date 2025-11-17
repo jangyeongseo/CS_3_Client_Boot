bed98b059264ccd39c49c8c98c261ec2a35e460c
heybin <tlszn10@naver.com>
2025-11-17 17:17:36 +0900

    CahtBotEntity 설정추가

--- src/main/java/com/kedu/project/chatbot/ChatBotDataEntity.java
+++ src/main/java/com/kedu/project/chatbot/ChatBotDataEntity.java
@@ -1,5 +1,20 @@
 package com.kedu.project.chatbot;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="chatbot_responses")
+@Getter
+@Setter
+@NoArgsConstructor
 public class ChatBotDataEntity {
-    
+    @Id
+    private int id;
+    private String trigger_text;
+    private String response_text;
 }
